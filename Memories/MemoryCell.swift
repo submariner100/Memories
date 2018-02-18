6fd743974e2cb4f7a19971688bd066c18325cd09
Michael Chapman <submariner100@gmail.com>
2018-02-18 20:18:01 +0000

    thirdcommit

---
+++ Memories/MemoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryCell.swift
+//  Memories
+//
+//  Created by Macbook on 18/02/2018.
+//  Copyright © 2018 Lodge Farm Apps. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+	@IBOutlet weak var imageView: UIImageView!
+}
